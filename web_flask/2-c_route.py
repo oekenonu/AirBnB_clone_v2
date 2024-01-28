f8e164b427de8e4e474bb41f618b4ec7d81a2b1f
obinna E <obinnaekenonu@gmail.com>
2024-01-28 12:38:06 +0000

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask web application.
+The application listens on port 5000 and displays 'Hello HBNB!'
+and HBNB
+and 'C ' followed by the value of the text variable
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
